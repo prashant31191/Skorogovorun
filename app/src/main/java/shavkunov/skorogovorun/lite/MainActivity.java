91a3fe0b8187f9995a755ae8aefdf017fc4145bf
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-11-14 16:49:29 +0500

    Add navigation bottom bar and replace colors.xml

--- app/src/main/java/shavkunov/skorogovorun/lite/MainActivity.java
+++ app/src/main/java/shavkunov/skorogovorun/lite/MainActivity.java
@@ -1,13 +1,43 @@
 package shavkunov.skorogovorun.lite;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottomBar)
+    BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setBottomBar();
+    }
+
+    private void setBottomBar() {
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId) {
+                    case R.id.tab_home:
+                        break;
+                    case R.id.tab_course:
+                        break;
+                    case R.id.tab_favorites:
+                        break;
+                    case R.id.tab_settings:
+                        break;
+                }
+            }
+        });
     }
 }
