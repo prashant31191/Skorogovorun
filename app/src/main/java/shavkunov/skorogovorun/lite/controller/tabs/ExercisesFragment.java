ef50b349690061c6317512ccd300549695617778
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-11-17 19:51:10 +0500

    Change card_exercises.xml

--- app/src/main/java/shavkunov/skorogovorun/lite/controller/tabs/ExercisesFragment.java
+++ app/src/main/java/shavkunov/skorogovorun/lite/controller/tabs/ExercisesFragment.java
@@ -1,17 +1,27 @@
 package shavkunov.skorogovorun.lite.controller.tabs;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.ColorFilterTransformation;
 import shavkunov.skorogovorun.lite.R;
+
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 public class ExercisesFragment extends Fragment {
 
@@ -45,10 +55,23 @@ public class ExercisesFragment extends Fragment {
         unbinder.unbind();
     }
 
-    private class ExercisesHolder extends RecyclerView.ViewHolder {
+    public class ExercisesHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.eCard_image)
+        ImageView eCardImage;
+
+        @BindView(R.id.eCard_title)
+        TextView eCardTitle;
+
+        @BindView(R.id.eCard_countTitle)
+        TextView eCardCount;
+
+        private View itemView;
 
         public ExercisesHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.itemView = itemView;
         }
     }
 
@@ -63,7 +86,20 @@ public class ExercisesFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ExercisesHolder holder, int position) {
+            switch (position) {
+                case 0:
+                    setImage(holder, R.drawable.forest);
+                    holder.eCardTitle.setText(R.string.tongueTwisters);
+                    holder.eCardCount.setText("100 скороговорок"); // В будущем будет расчет кол-ва скороговорок
+                    break;
+            }
+        }
 
+        private void setImage(ExercisesHolder holder, @DrawableRes int imageRes) {
+            Glide.with(holder.itemView)
+                    .load(imageRes)
+                    .apply(bitmapTransform(new ColorFilterTransformation(Color.argb(210, 66, 66, 66))))
+                    .into(holder.eCardImage);
         }
 
         @Override
