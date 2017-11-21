1c65f34cc8bad802ce304cd42e3001d2809c54bc
Alex Shavkunov <aleeexshavkunov@gmail.com>
2017-11-21 19:33:50 +0500

    Add Patter.java model

---
+++ app/src/main/java/shavkunov/skorogovorun/lite/model/Patter.java
@@ -0,0 +1,43 @@
+package shavkunov.skorogovorun.lite.model;
+
+public class Patter {
+
+    private String image;
+    private String title;
+    private String sounds;
+    private boolean isFavorite;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSounds() {
+        return sounds;
+    }
+
+    public void setSounds(String sounds) {
+        this.sounds = sounds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+
+
+}
