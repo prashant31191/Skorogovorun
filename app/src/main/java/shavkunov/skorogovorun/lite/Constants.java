ec2e7352665e7cabe7ace4dd132c9283d764dfc4
Alex Shavkunov <aleeexshavkunov@gmail.com>
2018-01-01 19:00:24 +0500

    Update the project

--- app/src/main/java/shavkunov/skorogovorun/lite/Constants.java
+++ app/src/main/java/shavkunov/skorogovorun/lite/Constants.java
@@ -12,7 +12,6 @@ public class Constants {
         private static final String LIGHT_PATTERS = "https://api.myjson.com/bins/128bib";
         private static final String MEDIUM_PATTERS = "https://api.myjson.com/bins/1cetyb";
         private static final String HARD_PATTERS = "https://api.myjson.com/bins/1h6asj";
-
         public static final String[] ARRAY_PATTERS = {ALL_PATTERS, LIGHT_PATTERS, MEDIUM_PATTERS,
                 HARD_PATTERS};
 
@@ -21,7 +20,6 @@ public class Constants {
         private static final String BREATH = "https://api.myjson.com/bins/xdakz";
         private static final String VOICE = "https://api.myjson.com/bins/16ydf7";
         private static final String DICTION = "https://api.myjson.com/bins/c1z4j";
-
         public static final String[] ARRAY_COURSES = {POSTURE, BREATH, VOICE, DICTION};
     }
 }
